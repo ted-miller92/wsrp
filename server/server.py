10c1b1409649cfbbd55f4910c56d5acfa693dd97
Ted Miller <65310255+ted-miller92@users.noreply.github.com>
2025-05-05 17:10:53 -0700

    Update server.py
    Added CORS origins

--- server/server.py
+++ server/server.py
@@ -47,7 +47,7 @@ app.config["JWT_TOKEN_LOCATION"] = ["cookies"]
 csrf = CSRFProtect(app)
 
 # Enable Cross-Origin Resource Sharing to allow requests from different domains
-CORS(app, resources={r"/api/*": {"origin": "http://localhost:5173", "supports_credentials": True}})
+CORS(app, resources={r"/api/*": {"origins": ["https://wsrp.space/", "https://www.wsrp.space/"], "supports_credentials": True}})
 
 # Database configuration
 # Note: It's a security best practice to store these in a .env file and not hard-code credentials
